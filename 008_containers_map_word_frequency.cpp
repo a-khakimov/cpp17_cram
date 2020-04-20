a8fb64cf9509df90da493db315a6d4002e5e4244
a-khakimov <techlinked@yandex.ru>
2020-04-20 12:20:36 +0500

    map word frequency

---
+++ 008_containers_map_word_frequency.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+std::string filter_punctuation(const std::string& s)
+{
+    const char* forbidden {".,:; "};
+    const auto idx_start (s.find_first_not_of(forbidden));
+    const auto idx_end (s.find_last_not_of(forbidden));
+    return s.substr(idx_start, idx_end - idx_start + 1);
+}
+
+
+#if true
+int main()
+{
+    std::map<std::string, std::size_t> words;
+    size_t max_word_len = 0;
+    std::string s;
+
+    while (std::cin >> s) {
+        auto filtered (filter_punctuation(s));
+        max_word_len = std::max<size_t>(max_word_len, filtered.length());
+        ++words[filtered];
+    }
+
+    std::vector<std::pair<std::string, std::size_t>> words_counts;
+    words_counts.reserve(words.size());
+    std::move(words.begin(), words.end(), std::back_inserter(words_counts));
+    std::sort(words_counts.begin(), words_counts.end(), [](const auto &a, const auto &b) {
+        return a.second > b.second;
+    });
+
+    std::cout << "# " << std::setw(max_word_len) << "<WORD>" << "#<COUNT>" << std::endl;
+    for (const auto& [word, count] : words_counts) {
+        std::cout << std::setw(max_word_len + 3) << word << " # " << count << std::endl;
+    }
+}
+#endif
--- cpp17_stl.pro
+++ cpp17_stl.pro
@@ -13,4 +13,5 @@ SOURCES += \
     005_containers_modify_map_key.cpp \
     006_containers_use_unordered_map_for_custom_types.cpp \
     007_containers_stack_rpm.cpp \
+    008_containers_map_word_frequency.cpp \
     main.cpp
