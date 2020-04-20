5820d3b5bc10d817401d14271bbe66a0bbeded79
a-khakimov <techlinked@yandex.ru>
2020-04-20 15:59:30 +0500

    custom stl compaliabilitible iterator

--- 011_iterators_custom_iterator.cpp
+++ 011_iterators_custom_iterator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 class num_iterator {
     int i;
@@ -13,7 +14,20 @@ public:
     bool operator!=(const num_iterator& other) const {
         return i != other.i;
     }
+    bool operator==(const num_iterator& other) const {
+        return i == other.i;
+    }
 };
+
+// Для совместимости кастомного итератора с stl-алгоритмати
+// Но фактически работает и без этого
+namespace std {
+template<>
+struct iterator_traits<num_iterator> {
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = int;
+};
+}
 
 class num_range {
     int a;
@@ -27,12 +41,19 @@ public:
     num_iterator end() const { return num_iterator{b}; }
 };
 
-#if true
+#if false
 int main()
 {
-    for (const auto& i : num_range {100, 110}) {
-        std::cout << i << ", ";
+    {
+        for (const auto& i : num_range {100, 110}) {
+            std::cout << i << ", ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
+    {
+        num_range r { 100, 110 };
+        auto [min_it, max_it] (std::minmax_element(std::begin(r), std::end(r)));
+        std::cout << *min_it << " - " << *max_it << std::endl;
+    }
 }
 #endif
